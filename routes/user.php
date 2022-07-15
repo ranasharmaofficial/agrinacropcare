14f1783e7792b27e263aef283f6dc3a094dc3d4b
Ankit Raj <nkflicktechnology@gmail.com>
2022-07-15 23:52:57 +0530

    User route page created

---
+++ routes/user.php
@@ -0,0 +1,14 @@
+<?php
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| User Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register user routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "user" middleware group. Now create something great!
+|
+*/
+
