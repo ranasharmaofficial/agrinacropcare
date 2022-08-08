320c7742770cd061620aee283aed8e7dadfb9a21
JK BISHWAS <85029965+jkbishwas@users.noreply.github.com>
2022-08-08 22:57:48 +0530

    change on edit profile page

--- resources/views/dashboard/editProfile.blade.php
+++ resources/views/dashboard/editProfile.blade.php
@@ -8,45 +8,44 @@
 
 @section('content')
     <main role="main" class="ion-content ion-myprofile">
-        <div class="mb-2 card bg-white profile-box text-center">
-            <div class="py-4 px-3 border-bottom">
-                <img alt="img" class="img-fluid mt-2 rounded-circle" src="https://i.pinimg.com/736x/d0/7a/f6/d07af684a67cd52d2f10acd6208db98f.jpg">
-                {{-- <h5 class="font-weight-bold text-dark mb-1 mt-4">{{ $LoggedUserInfo['name'] }}</h5> --}}
-            </div>
-            
-
-            {{-- @if ($user->role == 2)
-                <div class="overflow-hidden border-top p-3 d-flex justify-content-between align-items-center">
-                    <small class="text-secondary font-weight-bold">
-                        <i class="fa fa-credit-card" aria-hidden="true"></i>
-                        Wallet Amount:
-                    </small>
-                    <small class="text-primary font-weight-bold">Rs: 20/-</small>
+        <form action="" method="GET" enctype="multipart/form-data">
+            <div class="mb-2 card bg-white profile-box text-center">
+                <div class="card-header bg-success text-white font-weight-bold">
+                    Change Personal Details
                 </div>
-            @endif --}}
-        </div>
+                <div class="py-4 px-3 border-bottom">
+                    <img alt="img" class="img-fluid mt-2 rounded-circle"
+                        src="https://i.pinimg.com/736x/d0/7a/f6/d07af684a67cd52d2f10acd6208db98f.jpg">
+                    {{-- <h5 class="font-weight-bold text-dark mb-1 mt-4">{{ $LoggedUserInfo['name'] }}</h5> --}}
+                    <input type="file" class="form-control mt-2" name="editImage">
+                </div>
+            
+                <table class="table table-striped">
+                    <tbody>
+                        <tr>
+                            <th>Name:</th>
+                            <td><input type="text" class="form-control"></td>
+                        </tr>
+                        <tr>
+                            <th>Mobile:</th>
+                            <td><input type="text" class="form-control" value="9801785262" disabled></td>
+                        </tr>
+                        <tr>
+                            <th>Email:</th>
+                            <td><input type="text" class="form-control" value="example@gmail.com" disabled></td>
+                        </tr>
 
-        <div class="card">
-            <div class="card-header bg-indigo text-white font-weight-bold">
-                Personal Details
+                    </tbody>
+                    <tfoot>
+                        <tr >
+                            <td colspan="2">
+
+                                <button type="submit" class="btn btn-block text-white" style="background: #003049"> Update</button>
+                            </td>
+                        </tr>
+                    </tfoot>
+                </table>
             </div>
-            <table class="table table-striped">
-                <tbody>
-                    <tr>
-                        <th>Name:</th>
-                        {{-- <td>{{ $user->name }}</td> --}}
-                    </tr>
-                    <tr>
-                        <th>Mobile:</th>
-                        {{-- <td>{{ $user->mobile }}</td> --}}
-                    </tr>
-                    {{-- <tr>
-                        <th>Email:</th>
-                        <td>{{$user->email}}</td>
-                    </tr> --}}
-
-                </tbody>
-            </table>
-        </div>
+        </form>
     </main>
 @endsection
